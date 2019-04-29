1211b48bee345c4233697a7acb7462af78c98fb7
SRoller <sroller@justfab.com>
2019-04-29 12:10:52 -0400

    Initial Commit

---
+++ DB Deployer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DB Deployer
+//
+//  Created by Shawn Roller on 4/29/19.
+//  Copyright © 2019 Shawn Roller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DB Deployer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DB Deployer
+//
+//  Created by Shawn Roller on 4/29/19.
+//  Copyright © 2019 Shawn Roller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
