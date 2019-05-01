7afdb0f219093a158700b640444e4e3bc184fb0d
SRoller <sroller@justfab.com>
2019-05-01 17:43:16 -0400

    Adds shell script and refactors some UI elements.

--- DB Deployer/Controllers/MainViewController.swift
+++ DB Deployer/Controllers/MainViewController.swift
@@ -11,21 +11,12 @@ import Cocoa
 class MainViewController: NSViewController {
 
     let defaultDialogText = "Whatchu want sucka?!"
-    @IBOutlet weak var pathTextField: NSTextField!
-    @IBOutlet weak var pathButton: NSButton!
+    @IBOutlet weak var pathControl: NSPathControl!
     @IBOutlet weak var serverTableView: NSTableView!
-    @IBOutlet weak var dialogLabel: NSTextField!
+    @IBOutlet weak var dialogLabel: NSTextView!
     @IBOutlet weak var deployButton: NSButton!
     
     var preferences = Preferences()
-    override var representedObject: Any? {
-        didSet {
-            if let url = representedObject as? URL {
-                self.pathTextField.stringValue = "\(url)"
-                self.updateDialog()
-            }
-        }
-    }
     
     
     override func viewDidLoad() {
@@ -36,46 +27,105 @@ class MainViewController: NSViewController {
         self.serverTableView.dataSource = self
         self.serverTableView.target = self
         
-        // set delegate so we know when the text changes
-        self.pathTextField.delegate = self
-        
         // set defaults
         self.deployButton.isEnabled = false
-        self.dialogLabel.stringValue = self.defaultDialogText
+        self.dialogLabel.string = self.defaultDialogText
 
         self.show(preferences: self.preferences)
     }
     
     func show(preferences: Preferences) {
-        self.pathTextField.stringValue = preferences.defaultPath
+        self.pathControl.url = URL(fileURLWithPath: preferences.defaultPath)
     }
     
     func updateDialog() {
         let selectedRow = self.serverTableView.selectedRow
-        let text = self.pathTextField.stringValue
+        guard let url = self.pathControl.url else { return }
+        let text = url.path
         
         guard selectedRow >= 0 && selectedRow < self.preferences.dbConfigs.count && text.count > 0 else {
             self.deployButton.isEnabled = false
-            self.dialogLabel.stringValue = self.defaultDialogText
+            self.dialogLabel.string = self.defaultDialogText
             return
         }
         
         self.deployButton.isEnabled = true
         let name = self.preferences.dbConfigs[selectedRow].name
         
-        self.dialogLabel.stringValue = "I will deploy all the .sql scripts from \n\(text) \n\nto the database server \n\(name)\n\nShould I proceed?"
-    }
-
-    @IBAction func pathButtonClicked(_ sender: Any) {
-        
+        self.dialogLabel.string = "I will deploy all the .sql scripts from \n\(text) \n\nto the database server \n\(name)\n\nShould I proceed?"
     }
     
     @IBAction func deployButtonClicked(_ sender: Any) {
-        
+        guard let url = self.pathControl.url else { return }
+        let path = url.path
+        self.deployButton.isEnabled = false
+        let selectedConfig = self.preferences.dbConfigs[self.serverTableView.selectedRow]
+        self.buildScript(for: selectedConfig, folder: path)
     }
     
     @IBAction func deployMenuButtonClicked(_ sender: Any) {
         self.deployButtonClicked(sender)
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: Any) {
+//        if self.isRunning {
+//            self.buildTask.terminate()
+//        }
+    }
+    
+}
+
+// MARK: - Scripting
+extension MainViewController {
+    
+    func buildScript(for config: DBConfig, folder: String) {
+        let arguments = ["--login", config.name, folder, config.server, config.database, config.driver, config.trustedConnection ? "-E" : ""]
+        self.runScript(arguments: arguments)
+    }
+    
+    func runScript(arguments: [String]) {
+        let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+        taskQueue.async {
+            guard let path = Bundle.main.path(forResource: "DeployScript", ofType: "sh") else {
+                print("can't find build script")
+                return
+            }
+            
+            let deployTask = Process()
+            deployTask.launchPath = path
+            deployTask.arguments = arguments
+            deployTask.terminationHandler = {
+                task in
+                DispatchQueue.main.async(execute: {
+                    self.deployButton.isEnabled = true
+                })
+            }
+            
+            self.captureStdOut(deployTask)
+            deployTask.launch()
+            deployTask.waitUntilExit()
+        }
+    }
+    
+    func captureStdOut(_ task: Process) {
+        // setup output
+        let outputPipe = Pipe()
+        task.standardOutput = outputPipe
+        outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.NSFileHandleDataAvailable, object: outputPipe.fileHandleForReading, queue: nil, using: { (notification) in
+            let output = outputPipe.fileHandleForReading.availableData
+            let outputString = String(data: output, encoding: String.Encoding.utf8) ?? ""
+            
+            DispatchQueue.main.async(execute: {
+                let previousOutput = self.dialogLabel.string
+                let nextOutput = previousOutput + "\n" + outputString
+                self.dialogLabel.string = nextOutput
+                
+                let range = NSRange(location: nextOutput.count, length: 0)
+                self.dialogLabel.scrollRangeToVisible(range)
+            })
+            outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        })
     }
     
 }
