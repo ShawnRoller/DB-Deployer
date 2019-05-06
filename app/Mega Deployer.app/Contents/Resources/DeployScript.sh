9dab01ce6e3c0cd0f4b68ab5f0d88c1a3712471f
SRoller <sroller@justfab.com>
2019-05-06 10:23:12 -0400

    Adds app executable

---
+++ app/Mega Deployer.app/Contents/Resources/DeployScript.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#  DeployScript.sh
+#  DB Deployer
+#
+#  Created by Shawn Roller on 5/1/19.
+#  Copyright © 2019 Shawn Roller. All rights reserved.
+echo "*********************************"
+echo "Beginning deploy to ${2}"
+echo "*********************************"
+
+files=$(ls -v1 "${3}"/*.sql)
+
+for file in $files
+do
+    echo executing $file
+"${6}"/sqlcmd -S "${4}" -d "${5}" -i $file
+done
+
+echo "*********************************"
+echo "Deploys complete"
+echo "*********************************"
