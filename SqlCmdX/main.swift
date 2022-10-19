c20ec5401867e708d878f1c99c3dc7536461f935
Shawn Roller <sroller@techstyle.com>
2022-10-19 16:18:15 -0400

    Adds sqlcmdx target

---
+++ SqlCmdX/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SqlCmdX
+//
+//  Created by Shawn Roller on 10/19/22.
+//  Copyright © 2022 Shawn Roller. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
